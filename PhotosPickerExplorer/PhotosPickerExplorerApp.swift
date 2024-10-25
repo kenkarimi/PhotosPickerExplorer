611a85d25e0dace84e2c85b84375a81304dfb0b3
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-10-25 11:28:34 +0300

    Initial Commit

---
+++ PhotosPickerExplorer/PhotosPickerExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerExplorerApp.swift
+//  PhotosPickerExplorer
+//
+//  Created by Kennedy Karimi on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
